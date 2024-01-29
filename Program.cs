2f7f0f18c16b3a19023ca5fd48400aeee197df40
michaelunderwood <michaelunderwood@MICHAELUNDE2A0A>
2024-01-29 14:54:06 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿/* 1) The “Driver” class (the Program.cs class with the main method where the program begins)
+will:
+• Welcome the user to the game
+• Create a game board array to store the players’ choices
+• Ask each player in turn for their choice and update the game board array
+• Print the board by calling the method in the supporting class
+• Check for a winner by calling the method in the supporting class, and notify the players
+when a win has occurred and which player won the game
+
+2) The supporting class (with whichever name you choose) will:
+• Receive the “board” array from the driver class
+• Contain a method that prints the board based on the information passed to it
+• Contain a method that receives the game board array as input and returns if there is a
+winner and who it was \*

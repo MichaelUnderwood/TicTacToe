159034c3b1d2d9fb2c46f03bd3242f1026f80b38
n8shaw <nathanjshaw88@gmail.com>
2024-01-31 11:46:26 -0700

    I made an idea of what we can do in the main program, pretty archaic

--- Program.cs
+++ Program.cs
@@ -1,14 +1,129 @@
-﻿/* 1) The “Driver” class (the Program.cs class with the main method where the program begins)
-will:
-• Welcome the user to the game
+﻿/* Michael & Nate
+ * 1) Program.cs: 
+• Welcome the user to the game 
+    WelcomeUser
 • Create a game board array to store the players’ choices
+    GameBoardArray
 • Ask each player in turn for their choice and update the game board array
+    AskPlayerForChoice
 • Print the board by calling the method in the supporting class
+    PrintBoard (calls a method in the supporting class)
 • Check for a winner by calling the method in the supporting class, and notify the players
 when a win has occurred and which player won the game
+    CheckForWinner (calls a method in the supporting class)
+    NotifyWinner*/
 
-2) The supporting class (with whichever name you choose) will:
-• Receive the “board” array from the driver class
-• Contain a method that prints the board based on the information passed to it
-• Contain a method that receives the game board array as input and returns if there is a
-winner and who it was \*
+
+class Program
+{
+    static void Main()
+    {
+        bool winner = false;
+        string guess = "";
+        int counter = 0;
+        string theWinner = "";
+
+        //create game board
+        char[,] gameBoardArray = new char[3, 3];
+
+
+
+        //start of game
+        Console.WriteLine("Welcome to Byte Brigade's Tic-Tac-Toe of Destiny.");
+
+
+
+
+
+        do
+        {
+            int playerNum = 2;
+            char mark = 'O';
+
+            if (counter % 2 == 0)
+            {
+                playerNum = 1;
+            }
+            if (playerNum == 1)
+            {
+                mark = 'X';
+            }
+            //call the Print board method from other class
+            //PrintBoard();
+
+            Console.WriteLine($"Player {playerNum}'s Selection:");
+            guess = Console.ReadLine();
+
+
+            //check if valid input
+
+
+            //update the board
+            gameBoardArray = UpdateBoard(gameBoardArray, guess, mark);
+
+            //check if there is a winner
+            //need to assign theWinner(string) with what they return with here
+
+
+
+
+            counter += 1;
+
+        } while (!winner);
+
+
+        Console.WriteLine($"{theWinner} wone the game, come again sometime");
+
+
+
+
+    }
+
+    static char[,] UpdateBoard(char[,] gameBoardArray, string guess, char marker)
+    {
+        if (guess == "top left")
+        {
+            gameBoardArray[0, 0] = marker;
+        }
+        else if (guess == "top middle")
+        {
+            gameBoardArray[0, 1] = marker;
+        }
+        else if (guess == "top right")
+        {
+            gameBoardArray[0, 2] = marker;
+        }
+        if (guess == "left middle")
+        {
+            gameBoardArray[1, 0] = marker;
+        }
+        else if (guess == "middle")
+        {
+            gameBoardArray[1, 1] = marker;
+        }
+        else if (guess == "right middle")
+        {
+            gameBoardArray[1, 2] = marker;
+        }
+        if (guess == "bottom left")
+        {
+            gameBoardArray[2, 0] = marker;
+        }
+        else if (guess == "bottom middle")
+        {
+            gameBoardArray[2, 1] = marker;
+        }
+        else if (guess == "bottom right")
+        {
+            gameBoardArray[2, 2] = marker;
+        }
+
+        return gameBoardArray;
+    }
+
+
+
+
+
+
+}

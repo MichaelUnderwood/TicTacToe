7b777169741f3095526d0513bc0634cded4333a0
Michael Underwood <underwoodmichael6@gmail.com>
2024-01-30 22:50:21 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,45 @@
-﻿/* 1) The “Driver” class (the Program.cs class with the main method where the program begins)
-will:
-• Welcome the user to the game
+/* Michael & Nate
+ * 1) Program.cs: 
+• Welcome the user to the game 
+    WelcomeUser
 • Create a game board array to store the players’ choices
+    GameBoardArray
 • Ask each player in turn for their choice and update the game board array
+    AskPlayerForChoice
 • Print the board by calling the method in the supporting class
+    PrintBoard (calls a method in the supporting class)
 • Check for a winner by calling the method in the supporting class, and notify the players
 when a win has occurred and which player won the game
+    CheckForWinner (calls a method in the supporting class)
+    NotifyWinner*/
 
-2) The supporting class (with whichever name you choose) will:
-• Receive the “board” array from the driver class
-• Contain a method that prints the board based on the information passed to it
-• Contain a method that receives the game board array as input and returns if there is a
-winner and who it was \*
+
+class Program
+{
+    static void Main()
+    {
+        WelcomeUser();
+
+    }
+
+    static void WelcomeUser()
+    {
+        Console.WriteLine("Welcome to Byte Brigade's Tic-Tac-Toe of Destiny.");
+    }
+
+    static char[,] GameBoardArray()
+    {
+        // 
+        return new char[3, 3];
+    }
+
+    static void AskPlayerForChoice(char[,] gameBoardArray)
+    {
+        //stuff
+    }
+
+    static void NotifyWinner()
+    {
+        Console.WriteLine("Congrats!");
+    }
+}

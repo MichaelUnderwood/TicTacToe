30f29d47ab2367891b3c5fc817d4a90e8840b790
Michael Underwood <underwoodmichael6@gmail.com>
2024-01-30 22:50:50 -0700

    Update ToeClass.cs

--- ToeClass.cs
+++ ToeClass.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TicTacToe;
 
 namespace TicTacToe
 {
@@ -10,3 +12,16 @@ namespace TicTacToe
     {
     }
 }
+
+
+/* Ben & Jefferson
+2) ToeClass.cs will:
+• Receive the “board” array from the driver class
+    gameBoardArray[]
+• Contain a method that prints the board based on the information passed to it
+    PrintBoard
+• Contain a method that receives the game board array as input and returns if there is a
+winner and who it was \*
+    CheckForWinner */
+
+Im not sure if the comments acutally commit?

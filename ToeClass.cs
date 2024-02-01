5221ca8443910afb519abba2608775240c3787e8
Jefferson Lambert <83100592+mediumPuppy@users.noreply.github.com>
2024-01-31 20:37:37 -0700

    michael notes

--- ToeClass.cs
+++ ToeClass.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Ben & Jefferson
+2) ToeClass.cs will:
+• Receive the “board” array from the driver class
+    gameBoardArray[]
+• Contain a method that prints the board based on the information passed to it
+    PrintBoard
+• Contain a method that receives the game board array as input and returns if there is a
+winner and who it was \*
+    CheckForWinner */
 namespace TicTacToe
 {
     internal class ToeClass
     {
     }
 }
+

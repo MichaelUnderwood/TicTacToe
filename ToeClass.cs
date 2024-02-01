e871c96ebccb5a221f9ef70af201ad06a65ffc62
Jefferson Lambert <83100592+mediumPuppy@users.noreply.github.com>
2024-01-31 21:55:28 -0700

    ben and jefferson wildin

--- ToeClass.cs
+++ ToeClass.cs
@@ -13,10 +13,79 @@ using System.Threading.Tasks;
 • Contain a method that receives the game board array as input and returns if there is a
 winner and who it was \*
     CheckForWinner */
+
 namespace TicTacToe
 {
     internal class ToeClass
     {
+        public void PrintBoard(char[,] Board)
+        {
+            //This method doesn't return anything but prints out the board using Console.WriteLine
+            //based off what the board is
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    // just console.write so that it stays on the same line. added tab delimiter for spacing
+                    Console.Write(Board[row, col] + "\t");
+                }
+                //create new line for next row of matrix woo
+                Console.WriteLine();
+            }
+        }
+
+        public string CheckForWin(char[,] board)
+        {
+            // Check rows
+            for (int row = 0; row < 3; row++)
+            {
+                if (board[row, 0] == board[row, 1] && board[row, 1] == board[row, 2])
+                {
+                    if (board[row, 0] == 'X')
+                    {
+                        return "Player 1";
+                    }
+                    else if (board[row, 0] == 'O')
+                    {
+                        return "Player 2";
+                    }
+                }
+            }
+
+            // Check columns
+            for (int col = 0; col < 3; col++)
+            {
+                if (board[0, col] == board[1, col] && board[1, col] == board[2, col])
+                {
+                    if (board[0, col] == 'X')
+                    {
+                        return "Player 1";
+                    }
+                    else if (board[0, col] == 'O')
+                    {
+                        return "Player 2";
+                    }
+                }
+            }
+
+            // Check diagonals
+            if ((board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2]) ||
+                (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0]))
+            {
+                if (board[1, 1] == 'X')
+                {
+                    return "Player 1";
+                }
+                else if (board[1, 1] == 'O')
+                {
+                    return "Player 2";
+                }
+            }
+
+            // No winner
+            return "no winner";
+        }
     }
+}
 }
 
